13fc9d0625a7e34eae69e21c5db8f3185703ce60
yarden kivity <yarden.kivity@gmail.com>
2023-11-13 08:01:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Board width={4} height={4}></Board>
+}
+
+function Board( value ) {
+    let content = [];
+
+    for (let i = 0; i < value.width; i++) {
+        content.push(<div></div>)
+        for (let i = 0; i < value.height; i++) {
+            content.push(<Square></Square>)
+        }
+    }
+
+    return content
+}
+
+function Square() {
+    const [value, setValue]  = useState("")
+
+    const randomNumberInRange = (min, max) => {
+        return Math.floor(Math.random()
+            * (max - min + 1)) + min;
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            let num = randomNumberInRange(1, 5)
+
+            if (num === 2) {
+                setValue("X")
+            } else {
+                setValue("")
+            }
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return <button>{value}</button>
 }
 
 export default App;
